1f5737ed0da868fdf24d1bed360e3de0cbfec2f3
José Alarcón <dev.josealarcon@gmail.com>
2023-10-19 14:32:44 -0300

    Implement Domain

--- src/main/java/com/josealarcon/Application.java
+++ src/main/java/com/josealarcon/Application.java
@@ -1,4 +1,4 @@
-package com.josealarcon.APIREST.Santander.DIO;
+package com.josealarcon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
